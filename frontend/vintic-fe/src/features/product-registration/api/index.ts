9b0b21db46e88b72a7320ca48837fa72f6934c40
suyeon <yebbi2@ewhain.net>
2026-06-01 00:54:19 +0900

    [feat/#58] export용 index 구현

---
+++ frontend/vintic-fe/src/features/product-registration/api/index.ts
@@ -0,0 +1,15 @@
+export { analyzeProductImages } from './analyzeProductImages';
+export { analyzeRecommendedPrice } from './analyzeRecommendedPrice';
+export { registerProduct } from './registerProduct';
+
+export type {
+  ApiError,
+  ApiResponse,
+  AnalyzeProductImagesRequest,
+  AnalyzeProductImagesData,
+  AnalyzeRecommendedPriceRequest,
+  AnalyzeRecommendedPriceData,
+  RegisterProductRequest,
+  RegisterProductData,
+  MarketMatch,
+} from './types';
